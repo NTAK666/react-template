154ca9eedf8f26ab201f54712ffef781386e6750
NTAK666 <=>
2023-04-20 16:08:32 +0700

    app: reconfig api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,8 @@
 export const API = {
-  API_URL: 'https://jsonplaceholder.typicode.com',
-  get posts() {
-    return `${this.API_URL}/posts`;
-  },
+	API_URL: 'https://jsonplaceholder.typicode.com',
+	post: {
+		get get() {
+			return `${ API.API_URL }/posts`;
+		},
+	}
 };
--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -5,6 +5,6 @@ import { QUERY_KEY } from '@/config/query';
 import { Post } from '@/entities/post.entity';
 
 const getPosts = async (): Promise<Post[]> =>
-  axios.get(API.posts).then(res => res.data);
+	axios.get(API.post.get).then(res => res.data);
 
 export const usePosts = () => useQuery(QUERY_KEY.POSTS.ALL, getPosts);
